084064ad1cf0515e7d6c2a4d2cfc992ae4e14e0f
rogeriofabriciodev <60872513+rogeriofabriciodev@users.noreply.github.com>
2020-03-01 12:12:50 -0300

    Router OK
    Router OK

--- src/components/nav/MenuSignIn.jsx
+++ src/components/nav/MenuSignIn.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import { NavLink } from 'react-router-dom'
 import { Navbar, NavDropdown, Nav, Image, Row, Col } from 'react-bootstrap'
 
 const MenuSignIn = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Model-APP</Navbar.Brand>
+        <Navbar.Brand href="/">Model-APP</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link><NavLink to='pageone' className="text-secondary text-decoration-none">PageOne</NavLink></Nav.Link>
+            <Nav.Link><NavLink to='/' className="text-secondary text-decoration-none">Pricing</NavLink></Nav.Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1"><NavLink to='/' className="text-secondary text-decoration-none">Action</NavLink></NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2"><NavLink to='/' className="text-secondary text-decoration-none">Another action</NavLink></NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3"><NavLink to='/' className="text-secondary text-decoration-none">Something</NavLink></NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4"><NavLink to='/' className="text-secondary text-decoration-none">Separated link</NavLink></NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
